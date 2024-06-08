f43a8b1b7b216a40c827144cd88d89b745c20ee0
vanquang <nguyenvanquangq013@gmail.com>
2024-06-08 15:36:35 +0700

    refactor

--- src/main/java/com/facenet/demoVNPAYAMS/modelWriteFile/SHBModel.java
+++ src/main/java/com/facenet/demoVNPAYAMS/modelWriteFile/SHBModel.java
@@ -1,51 +1,55 @@
-    package com.facenet.demoVNPAYAMS.modelWriteFile;
+package com.facenet.demoVNPAYAMS.modelWriteFile;
 
-    import lombok.Getter;
-    import lombok.NoArgsConstructor;
-    import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    import java.time.LocalDateTime;
-    import java.util.concurrent.ThreadLocalRandom;
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    public class SHBModel extends  GeneralModel{
-        private String referenceNumber;
-        private LocalDateTime accountingDay;
-        private String description;
+@Getter
+@Setter
+@NoArgsConstructor
+public class SHBModel extends GeneralModel {
+  private String referenceNumber;
+  private LocalDateTime accountingDay;
+  private String description;
 
-        public SHBModel(LocalDateTime transactionDate, double creditAmount, double debitAmount, String service,
-                        String referenceNumber, LocalDateTime accountingDay, String description) {
-            super(transactionDate, creditAmount, debitAmount, service);
-            this.referenceNumber = referenceNumber;
-            this.accountingDay = accountingDay;
-            this.description = description;
-        }
-        public static SHBModel createRandomSHBModel() {
-            LocalDateTime transactionDate = GeneralGenerateInterface.generateRandomDate();
-            double creditAmount = GeneralGenerateInterface.generateRandomAmount();;
-            double debitAmount = GeneralGenerateInterface.generateRandomAmount();;
-            String service = GeneralGenerateInterface.generateRandomService();// You may set a default service here or generate randomly as well
-            String referenceNumber = GeneralGenerateInterface.generateRandomReferenceNumber();
-            LocalDateTime accountingDay = generateRandomDateTime();
-            String description = generateRandomDescription();
+  public SHBModel(LocalDateTime transactionDate, double creditAmount, double debitAmount, String service,
+                  String referenceNumber, LocalDateTime accountingDay, String description) {
+    super(transactionDate, creditAmount, debitAmount, service);
+    this.referenceNumber = referenceNumber;
+    this.accountingDay = accountingDay;
+    this.description = description;
+  }
 
-            return new SHBModel(transactionDate, creditAmount, debitAmount, service,
-                    referenceNumber, accountingDay, description);
-        }
+  public static SHBModel createRandomSHBModel() {
+    LocalDateTime transactionDate = GeneralGenerateInterface.generateRandomDate();
+    double creditAmount = GeneralGenerateInterface.generateRandomAmount();
+    ;
+    double debitAmount = GeneralGenerateInterface.generateRandomAmount();
+    ;
+    String service = GeneralGenerateInterface.generateRandomService();// You may set a default service here or generate randomly as well
+    String referenceNumber = GeneralGenerateInterface.generateRandomReferenceNumber();
+    LocalDateTime accountingDay = generateRandomDateTime();
+    String description = generateRandomDescription();
 
-        // Helper methods for generating random values
-        private static LocalDateTime generateRandomDateTime() {
-            long minDay = LocalDateTime.of(2024, 2, 1, 0, 0).atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli();
-            long maxDay = LocalDateTime.of(2024, 2, 29, 23, 59).atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli();
-            long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
-            return LocalDateTime.ofEpochSecond(randomDay / 1000, 0, java.time.ZoneOffset.UTC);
-        }
+    return new SHBModel(
+          transactionDate, creditAmount, debitAmount, service,
+          referenceNumber, accountingDay, description);
+  }
 
-        private static String generateRandomDescription() {
-            // Generate random description as needed
-            return "TT CAC GD TOPUP NGAY " + generateRandomDateTime();
-        }
+  // Helper methods for generating random values
+  private static LocalDateTime generateRandomDateTime() {
+    long minDay = LocalDateTime.of(2024, 2, 1, 0, 0).atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli();
+    long maxDay = LocalDateTime.of(2024, 2, 29, 23, 59).atZone(java.time.ZoneId.systemDefault()).toInstant().toEpochMilli();
+    long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
+    return LocalDateTime.ofEpochSecond(randomDay / 1000, 0, java.time.ZoneOffset.UTC);
+  }
 
-    }
+  private static String generateRandomDescription() {
+    // Generate random description as needed
+    return "TT CAC GD TOPUP NGAY " + generateRandomDateTime();
+  }
+
+}
